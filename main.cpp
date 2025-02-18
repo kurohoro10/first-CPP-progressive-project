6791a26880fc4a80842e6275d88f1663fe543884
kurohoro10 <kurohoro10@gmail.com>
2025-02-18 16:40:37 +0800

    Add variables

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string character_name = "Thomas";
+    int character_age = 10;
+    cout << "There was once a man name " << character_name << endl;
+    cout << "He was " << character_age << " years old." << endl;
+    character_name += 106;
+    character_name += 111;
+    character_name += 104;
+    character_name += 110;
+    cout << "He like the name " << character_name << endl;
+    cout << "But did not like being " << character_age << endl;
     return 0;
 }
