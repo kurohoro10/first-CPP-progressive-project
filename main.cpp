c1fffde26cc5013f4e1ecd99880ea5aaf2a3bfaf
kurohoro10 <kurohoro10@gmail.com>
2025-02-19 16:44:51 +0800

    Adding endline using backslash n

--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@ int main()
 {
     string character_name = "Thomas";
     int character_age = 10;
-    cout << "There was once a man name " << character_name << endl;
-    cout << "He was " << character_age << " years old." << endl;
+    cout << "There was once a man name " << character_name << "\n" ;
+    cout << "He was " << character_age << " years old. \n"  ;
     character_name += 106;
     character_name += 111;
     character_name += 104;
     character_name += 110;
     cout << "He like the name " << character_name << endl;
     cout << "But did not like being " << character_age << endl;
+    cout << 1 / 1 << endl;
     return 0;
 }
