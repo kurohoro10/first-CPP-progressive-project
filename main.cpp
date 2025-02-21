1bf90bfe0a57de0827b552aa307fd095ac17d997
kurohoro10 <kurohoro10@gmail.com>
2025-02-21 18:02:42 +0800

    Created script for testing time library.

--- main.cpp
+++ main.cpp
@@ -1,54 +1,20 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-#include <vector>
-
+#include <ctime>
 using namespace std;
 
-int main()
-{
-    bool ships[4][4] = {
-        {0, 1, 1, 0},
-        {0, 0, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 1, 0},
-    };
 
-    //Keep track of how many hits the player has and how many turns they have played in the variables
-    int hits = 0;
-    int numberOfTurns = 0;
+int main() {
+    clock_t before = clock();
 
-    //Allow the player to keep going until they have hit all four ships
-    while (hits < 4) {
-        int row, column;
+    int k = 0;
 
-        cout << "Selecting coordinates\n";
-
-        //Ask the player for a row
-        cout << "Choose a row  number between 0 and 3: ";
-        cin >> row;
-
-        //Ask the player for a column
-        cout << "Choose a column number between 0 and 3: ";
-        cin >> column;
-
-        //Check if a ship exists in those coordinates
-        if (ships[row][column]) {
-            ships[row][column] = 0;
-
-            //Increase the hit counter
-            hits++;
-
-            cout << "Hit! " << (4-hits) << " left.\n\n";
-        } else {
-            //Tell the player that they missed
-            cout << "Miss\n\n";
-        }
-
-        numberOfTurns++;
+    for(int i = 0; i < 100000; i++) {
+        k += i;
     }
 
-    cout << "Victory!\n";
-    cout << "You won in  " << numberOfTurns << " turns";
+    clock_t duration = clock() - before;
+
+    cout << "Duration: " << (float)duration / CLOCKS_PER_SEC << " seconds";
+
     return 0;
 }
